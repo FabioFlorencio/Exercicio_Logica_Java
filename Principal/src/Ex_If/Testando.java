498e1727ee28666aa3975932214d91e0d7c30c6f
Fabio Florencio <fabioflorencioxyz@gmail.com>
2022-08-24 19:14:31 -0300

    Primeiro teste java

---
+++ Principal/src/Ex_If/Testando.java
@@ -0,0 +1,16 @@
+package Ex_If;
+
+import java.util.Scanner; 
+
+public class Testando {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.print("Diga seu nome:");
+		String nome = scan.nextLine();
+		
+		System.out.println(nome);
+
+	}
+}
